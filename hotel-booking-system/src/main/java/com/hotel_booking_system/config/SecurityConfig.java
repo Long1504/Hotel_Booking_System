20f69f2942703cf1daa36c3bebc9923cc69f786f
long1504 <pvlong1504@gmail.com>
2026-04-07 17:19:05 +0700

    update base url

--- hotel-booking-system/src/main/java/com/hotel_booking_system/config/SecurityConfig.java
+++ hotel-booking-system/src/main/java/com/hotel_booking_system/config/SecurityConfig.java
@@ -198,8 +198,9 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://127.0.0.1:5500", "http://localhost:5500", "http://127.0.0.1:5501", "http://localhost:5501", "https://nadia-nonstudied-lilianna.ngrok-free.dev"));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        //configuration.setAllowedOrigins(List.of("http://127.0.0.1:5500", "http://localhost:5500", "http://127.0.0.1:5501", "http://localhost:5501", "https://nadia-nonstudied-lilianna.ngrok-free.dev"));
+        configuration.setAllowedOriginPatterns(List.of("*"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true); //Cho phép gửi cookie/token nếu cần
 
